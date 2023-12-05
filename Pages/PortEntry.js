5b554f072a58a6397de296e6b68bf6c74f3bb71c
Alan <alan.phan@knights.ucf.edu>
2023-12-05 18:58:56 -0500

    added static to PortEntry.js

--- Pages/PortEntry.js
+++ Pages/PortEntry.js
@@ -17,6 +17,7 @@ const app = express();
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use(express.static(__dirname + '/public'));
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/PortEntry.html");
