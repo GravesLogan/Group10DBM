0d98f4f9ad9000b7b8b7324afaf5d4f65088539a
Andrew Miller <an873997@ucf.edu>
2023-12-05 19:43:21 -0500

    fixed stuff
    changed pathing, moved stuff around, renamed main.html to index.html

--- Pages/PortEntry.js
+++ Pages/PortEntry.js
@@ -1,6 +1,4 @@
 const express = require('express');
-var http = require('http');
-var url = require('url');
 var fs = require('fs');
 const cors = require("cors")
 const mysql = require('mysql')
@@ -9,45 +7,34 @@ const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'mysql',
-  database: 'testDB'
+  database: 'Port_Management_System'
 })
 
 connection.connect()
 
-http.createServer(function (req, res) {
-    const app = express();
+const app = express();
 
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.static(__dirname));
 
-    app.get("/", (req, res) => {
-        res.sendFile(__dirname + "/PortEntry.html");
+console.log(__dirname)
+
+app.get("/TEST", (req, res) => {
+    const query = 'SELECT * FROM Truck';
+    connection.query(query, (error, results) => {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(results);
+
+            results.forEach(el => {
+                console.log(el.TruckID)
+            });
+        }
     });
+})
 
-    app.get("/TEST", (req, res) => {
-        console.log("A")
 
-        const query = 'SELECT * FROM Trucks';
-        connection.query(query, (error, results) => {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log(results);
-            }
-        });
-    })
-
-    var q = url.parse(req.url, true);
-    var filename = "." + q.pathname;
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-        } 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080);
+app.listen(8080, () => console.log(`Listening on 8080`));
